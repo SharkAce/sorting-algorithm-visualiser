ff36dda622fd20ac9325e6aa9b463465baa514db
SharkAce <charles.marchildon2@gmail.com>
2021-09-27 22:36:12 -0400

    init

--- sketch.js
+++ sketch.js
@@ -1,27 +1,41 @@
-let i=0
-let speed = 15
 let size = 15
-let nums = randomArr(size)
-let frames = longInsertionSort(nums)
+function init(){
+window.frame = 0
+window.nums = randomArr(size)
+window.frames = longInsertionSort(nums)
+window.reset = 0
+window.run = 0
+}
+init()
 
 function setup(){
 
   createCanvas(1400, 800)
-  textSize(20)
+  textAlign(CENTER)
 }
 function draw(){
-    frameRate(speed)
+  let speed = parseInt(document.getElementById("speed").value, 10)
+  frameRate(speed || 40)
   background(53)
   strokeWeight(1.5)
+  fill(250)
+  textSize(40)
+  text("Sorting algorithm visualiser",850,100)
   fill(70)
-  rect(20,70,350,600,10)
+  rect(20,90,350,600,10)
   fill(200)
 
+  if (reset){
+    init()
+  }
 
     let posX = 500
-    for (data of frames[i]){
-      rect(posX,600,50,-data)
+    for (data of frames[frame]){
+      rect(posX,640,50,-data*2.5)
+      textSize(20)
+      text(data, posX+25, 680)
     posX += 60
     }
-    if (frames.length > i+1){i++}
+    frames.length > frame+1 && run ? frame++ : run = 0
+
 }
